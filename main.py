9dc780b0514930e29ed25d0bbdff4e73da21bbf6
A.Artyushin <artyush1n3@mail.ru>
2025-07-03 13:37:39 +0300

    Настроено использование виртуального окружения

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from dotenv import load_dotenv
 from gigachat import GigaChat as GiChat
+load_dotenv()
 
 question = "Какой плащ был у Понтия Пилата?"
-
-load_dotenv()
 
 llmG = GiChat()
 
 resG = llmG.chat(question)
 
 print(resG.choices[0].message.content)
-print(resL.content)
