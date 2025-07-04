a0d4b1c03835e5c6c4f6fa870ceedf53e8186fd3
A.Artyushin <artyush1n3@mail.ru>
2025-07-04 13:17:22 +0300

    Отделили системный промпт от констекста с вопросом.

---
+++ sandbox/dialog.py
@@ -0,0 +1,38 @@
+import chromadb
+from  gigachat import GigaChat
+from gigachat.models import Chat, Messages, MessagesRole
+from dotenv import load_dotenv
+from os.path import normpath, join, dirname
+
+load_dotenv()
+llm = GigaChat()
+path_to_db = normpath(join(dirname(__file__), '..', 'db'))
+db = chromadb.PersistentClient(path=path_to_db)
+
+collection = db.get_or_create_collection(name="abc")
+
+system_prompt = """Представь, что ты ассистент поддержки, отвечающий на важные
+    мне вопросы исключительно на русском языке, на основании определенного контекста.
+    Если информации нет в контексте, то напиши кратко: "Не могу помочь с этим вопросом" и ничего больше,
+    а если есть, то вставь кусок из контекста. При ответе на вопрос также ни в коем случае не упоминай, 
+    что существует какой-то контекст.
+    """
+
+question = input("Введите ваш вопрос: ")
+docs = collection.query(query_texts=[question], n_results=5)
+all_context = '\n\n'.join(docs['documents'][0])
+
+messages = [
+    Messages(
+        role=MessagesRole.SYSTEM,
+        content = system_prompt
+    ),
+    Messages(
+        role=MessagesRole.USER,
+        content = f"Контекст: {all_context} \n Вопрос: {question} " + "\nТвой ответ: "
+    )
+]
+
+call_gigachat = llm.chat(Chat(messages=messages))
+
+print(call_gigachat.choices[0].message.content)
