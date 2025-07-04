ec804721c843eebfbfc6f5296b1a659dc708f4fc
A.Artyushin <artyush1n3@mail.ru>
2025-07-04 12:26:42 +0300

    Реализован полный RAG-запрос

--- sandbox/chroma.py
+++ sandbox/chroma.py
@@ -7,12 +7,13 @@ from dotenv import load_dotenv
 from os.path import normpath, join, dirname
 
 load_dotenv()
-
+llm = GigaChat()
 path_to_db = normpath(join(dirname(__file__), '..', 'db'))
 
 ## Подгрузка БД Chroma
 # embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 db = chromadb.PersistentClient(path=path_to_db)
+
 collection = db.get_or_create_collection(name="abc")
 
 # db = Chroma(persist_directory=path_to_db, collection_name="abc", embedding_function=DefaultEmbeddingFunction)
@@ -20,16 +21,15 @@ system_prompt = """Представь, что ты ассистент подде
     мне вопросы исключительно на русском языке, на основании определенного контекста.
     Если информации нет в контексте, то напиши кратко: "Не могу помочь с этим вопросом" и ничего больше,
     а если есть, то вставь кусок из контекста. При ответе на вопрос также ни в коем случае не упоминай, 
-    что существует какой-то контекст."""
+    что существует какой-то контекст.
+    Отвечай стихами"""
 
 question = input("Введите ваш вопрос: ")
 
 # 5 контекстов
 docs = collection.query(query_texts=[question], n_results=5)
 all_context = '\n\n'.join(docs['documents'][0])
-print(all_context)
-#llm = GigaChat()
 
-#all_prompt = f"{system_prompt} \n Контекст: {all_context} \n Вопрос: {question} " + "\nТвой ответ: "
+all_prompt = f"{system_prompt} \n Контекст: {all_context} \n Вопрос: {question} " + "\nТвой ответ: "
 
-#print(llm.invoke(all_prompt).content)
+print(llm.invoke(all_prompt).content)
