cc6ed1c2330b9200be7919bc2dfe8e9cb2f4c55f
A.Artyushin <artyush1n3@mail.ru>
2025-07-07 13:32:42 +0300

    Вывод размерности эмбеддингов "Embeddings" & "EmbeddingsGigaR" в консоль

---
+++ sandbox/GigaEmbeddings.py
@@ -0,0 +1,17 @@
+from gigachat import GigaChat
+from langchain_gigachat import GigaChatEmbeddings
+from dotenv import load_dotenv
+from os.path import normpath, join, dirname
+import chromadb
+
+load_dotenv()
+llm = GigaChat()
+path_to_db = normpath(join(dirname(__file__), '..', 'db'))
+db = chromadb.PersistentClient(path=path_to_db)
+
+
+embeddings = llm.embeddings(texts=["Привет"], model="Embeddings")
+embeddings1 = llm.embeddings(texts=["Привет"], model="EmbeddingsGigaR")
+
+print(len(embeddings.data[0].embedding))
+print(len(embeddings1.data[0].embedding))
