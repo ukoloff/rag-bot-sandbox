fb616bf7569438c27bc943440a2a7e5adc179d4a
A.Artyushin <artyush1n3@mail.ru>
2025-07-08 13:40:01 +0300

    Заполнение коллекции

---
+++ sandbox/fill_bd.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from os.path import normpath, join, dirname
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from embinterface import get_collection
+
+load_dotenv()
+
+path_to_txt = join(dirname(__file__), 'мастер_и_маргарита.txt')
+loader = TextLoader(path_to_txt, encoding="UTF-8")
+document = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+texts = text_splitter.split_documents(document)
+new_texts = [doc.page_content for doc in texts]
+
+for coll in get_collection():
+    if len(coll.get(limit=1)) > 0:
+        continue
+    coll.add(
+        ids=[f"{i}" for i in range(len(new_texts))],
+        documents=new_texts
+    )
