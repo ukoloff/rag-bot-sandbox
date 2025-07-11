7f106c5d1b131a4c92f9844b43e7d49b49ef1881
A.Artyushin <artyush1n3@mail.ru>
2025-07-11 12:10:33 +0300

    Сделан единичный RAG-запрос c помощи LangChain

--- sandbox/chain/chain_manually.py
+++ sandbox/chain/chain_manually.py
@@ -1,53 +1,37 @@
 from dotenv import load_dotenv
-from gigachat import GigaChat
 from os.path import normpath, join, dirname
-import chromadb
-from sandbox.embinterface import GigaChatEmb
-from gigachat.models import Chat, Messages, MessagesRole
-
-from sandbox.translate import template
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_gigachat import GigaChat, GigaChatEmbeddings
 
 load_dotenv()
 llm = GigaChat()
-path_to_db = normpath(join(dirname(__file__), '..', '..', 'db'))
-db = chromadb.PersistentClient(path=path_to_db)
-# print(db.list_collections())
-collection = db.get_or_create_collection(name="EmbeddingsGigaR", embedding_function=GigaChatEmb('EmbeddingsGigaR'))
-# question = input("Вопрос: ")
-# print(llm.embeddings(texts=['hi!']).data[0])
-# print(collection.query(query_texts='hi!', n_results=3))
+path_to_db = normpath(join(dirname(__file__), '..', '..', 'chroma.kb'))
+db = Chroma(collection_name="kb.gigaR", embedding_function=GigaChatEmbeddings(model='EmbeddingsGigaR'), persist_directory=path_to_db)
+retriever = db.as_retriever()
 
-template = """
-    Представь, что ты ассистент поддержки, отвечающий на важные
+system_prompt = """Представь, что ты ассистент поддержки, отвечающий на важные
     мне вопросы исключительно на русском языке, на основании определенного контекста.
     Если информации нет в контексте, то напиши кратко: "Не могу помочь с этим вопросом" и ничего больше,
     а если есть, то вставь кусок из контекста. При ответе на вопрос также ни в коем случае не упоминай, 
     что существует какой-то контекст.
-"""
+    Контекст: {context}
+    Вопрос: {question}
+    Твой ответ:"""
 
-messages = [
-    Messages(
-        role=MessagesRole.SYSTEM,
-        content = template
-    ),
-]
+def join(docs):
+    s = '\n\n'.join(doc.page_content for doc in docs)
+    return s
 
-while True:
-    system_prompt = """Представь, что ты ассистент поддержки, отвечающий на важные
-        мне вопросы исключительно на русском языке, на основании определенного контекста.
-        Если информации нет в контексте, то напиши кратко: "Не могу помочь с этим вопросом" и ничего больше,
-        а если есть, то вставь кусок из контекста. При ответе на вопрос также ни в коем случае не упоминай, 
-        что существует какой-то контекст.
-        Контекст: {context}
-        Вопрос: {question}
-        Твой ответ:"""
-    prompt = ChatPromptTemplate.from_template(system_prompt)
+prompt = ChatPromptTemplate.from_template(system_prompt)
+question = input("Введите ваш вопрос: ")
 
-    return (
-            {
-                "context": db.as_retriever(), "question": RunnablePassthrough()
-            }
-            | prompt
-            | llm
-        # | StrOutputParser
-    )
+chain = (
+    {
+        "context": retriever | join, "question": RunnablePassthrough()
+    }
+    | prompt
+    | llm
+)
+print(chain.invoke(question).content)
