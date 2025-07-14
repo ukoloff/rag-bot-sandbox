55e556b6a2ff8294e399085e606819631c8b0906
A.Artyushin <artyush1n3@mail.ru>
2025-07-14 09:33:15 +0300

    Сделана асинхронная версия для обновления промпта

--- sandbox/prompt.py
+++ sandbox/prompt.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import aiofiles
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda
 from os.path import normpath, join, dirname
@@ -15,7 +18,17 @@ def my_prompt(data):
         text = f.read()
     return ChatPromptTemplate.from_template(text + system_prompt).invoke(data)
 
+async def amy_prompt(data):
+    async with aiofiles.open(file=path_to_file ,encoding='UTF-8') as f:
+        text = await f.read()
+    return await ChatPromptTemplate.from_template(text + system_prompt).ainvoke(data)
 
-p2 = RunnableLambda(my_prompt)
+p2 = RunnableLambda(my_prompt, amy_prompt)
 a2 = p2.invoke(input)
 print(a2)
+
+async def main():
+    aa = await p2.ainvoke(input)
+    print(aa)
+
+asyncio.run(main())
