5747e834f3d98eb386e21457198411aefa9b4bfb
A.Artyushin <artyush1n3@mail.ru>
2025-07-15 08:28:38 +0300

    Убран лишний import и системный промпт

--- sandbox/tgbot/bot.py
+++ sandbox/tgbot/bot.py
@@ -3,7 +3,6 @@ from os import getenv
 
 from os.path import normpath, join, dirname
 from langchain_chroma import Chroma
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_gigachat import GigaChat, GigaChatEmbeddings
 from gigachat.exceptions import ResponseError
@@ -26,19 +25,9 @@ path_to_db = normpath(join(dirname(__file__), '..', '..', 'chroma.kb'))
 db = Chroma(collection_name="kb.gigaRtext", embedding_function=GigaChatEmbeddings(model='Embeddings'), persist_directory=path_to_db)
 retriever = db.as_retriever()
 
-system_prompt = """Представь, что ты ассистент поддержки, отвечающий на важные
-    мне вопросы исключительно на русском языке, на основании контекста, написанного ниже.
-    Если не знаешь ответа, то напиши просто: "Не могу помочь с этим вопросом".
-    Отвечай языком Михайлы Васильича Ломоносова.
-    Контекст: {context}
-    Вопрос: {question}
-    Твой ответ: """
-
 def join(docs):
     s = '\n\n'.join(doc.page_content for doc in docs)
     return s
-
-prompt = ChatPromptTemplate.from_template(system_prompt)
 
 chain = (
     {
