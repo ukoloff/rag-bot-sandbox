760846d7f25624f9b08e0c398f9b3005e9d5a546
A.Artyushin <artyush1n3@mail.ru>
2025-07-16 12:39:26 +0300

    Finished parser for PDF files with 'minimum chunk size' as a parameter

--- sandbox/pdf_parser.py
+++ sandbox/pdf_parser.py
@@ -4,6 +4,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_gigachat import GigaChatEmbeddings
 from langchain_chroma import Chroma
+from langchain.schema import Document
 from os.path import normpath, join, dirname
 from dotenv import load_dotenv
 
@@ -25,13 +26,27 @@ def load_document(pdf_folder_path):
                 print(e)
     return documents
 
-def split_text(documents, chunk_size=1000, chunk_overlap=200):
+def split_text(documents, chunk_size=1000, chunk_overlap=200, min_chunk_size=200):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
     )
     texts = text_splitter.split_documents(documents)
-    return texts
+
+    filtered_texts = []
+    i = 0
+    while i < len(texts):
+        current_text = texts[i]
+        if len(current_text.page_content) < min_chunk_size and i < len(texts) - 1:
+            merged_content = current_text.page_content + " " + texts[i + 1].page_content
+            merged_metadata = {**current_text.metadata, **texts[i + 1].metadata}
+            merged_doc = Document(page_content=merged_content, metadata=merged_metadata)
+            filtered_texts.append(merged_doc)
+            i += 2
+        else:
+            filtered_texts.append(current_text)
+            i += 1
+    return filtered_texts
 
 def update_vector_db(texts, persist_dir, collection_name):
     embeddings = GigaChatEmbeddings()
