b3b2579b0cd994af27b27dd7505a6fc410856109
A.Artyushin <artyush1n3@mail.ru>
2025-07-16 12:05:10 +0300

    Удаление данных из коллекции по параметру 'source'

---
+++ sandbox/tgbot/collection_deleting.py
@@ -0,0 +1,16 @@
+import chromadb
+from os.path import normpath, join, dirname
+import os
+
+pdf_folder_path = normpath(join(dirname(__file__), '..', '..', 'pdfs'))
+
+db_path = normpath(join(dirname(__file__), '..', '..', 'chroma.kb'))
+client = chromadb.PersistentClient(path=db_path)
+collection = client.get_collection(name='kb.gigaRtext')
+# print(collection.get(include=['metadatas']))
+
+for file in os.listdir(pdf_folder_path):
+    if file.endswith('.pdf'):
+        file_path = join(pdf_folder_path, file)
+        print(file_path)
+        collection.delete(where={'source': file_path})
