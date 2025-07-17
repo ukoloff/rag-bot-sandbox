e75a5767d7a4a973644289bab1c7eb5ff34b30e8
A.Artyushin <artyush1n3@mail.ru>
2025-07-17 08:51:11 +0300

    Проверка на максимальную вместимость окна

---
+++ sandbox/predel.py
@@ -0,0 +1,34 @@
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import TextLoader
+from langchain_gigachat import GigaChat
+from dotenv import load_dotenv
+from os.path import normpath, join, dirname
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+llm = GigaChat()
+
+path_to_txt = normpath(join(dirname(__file__), 'мастер_и_маргарита.txt'))
+
+path_to_write = normpath(join(dirname(__file__), 'output', 'мастер_и_маргарита_output.txt'))
+
+def load_document(file_path):
+    loader = TextLoader(file_path, encoding="UTF-8")
+    documents = loader.load()
+    return documents
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", "Напишите краткое резюме следующего текста:\\n\\n{context}")]
+)
+
+# Создание цепочки
+chain = create_stuff_documents_chain(llm, prompt)
+
+# Вызов цепочки
+result = chain.invoke({"context": load_document(path_to_txt)})
+
+with open(path_to_write, 'w', encoding='UTF-8') as f:
+    f.write(result)
+
+
+# print(len(load_document(path_to_txt)[0].page_content))
