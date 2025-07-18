85335e469e48b0e695e7385bd751578579e1c72d
A.Artyushin <artyush1n3@mail.ru>
2025-07-18 09:56:45 +0300

    Раскомментирована проверка на то, что вектора (эмбеддинги) одинаковые

--- sandbox/chroma_copy/changing_db.py
+++ sandbox/chroma_copy/changing_db.py
@@ -4,7 +4,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from os.path import normpath, join, dirname
 
 path_to_existing_db = normpath(join(dirname(__file__), '..', '..', 'db'))
-path_to_new_db = normpath(join(dirname(__file__), '..', '..', 'db'))
+path_to_new_db = normpath(join(dirname(__file__), '..', '..', 'new_db'))
 
 embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 
@@ -25,12 +25,12 @@ def add_to_collection(path, collection_name, docs, embedding=embeddings):
 
 documents_from_db = get_from_collection(path=path_to_existing_db, collection_name='default')
 # print(documents_from_db)
-add_to_collection(path=path_to_new_db, collection_name='copy_to', docs=documents_from_db)
+add_to_collection(path=path_to_new_db, collection_name='copy_to2', docs=documents_from_db)
 
-# first_db = Chroma(persist_directory=path_to_existing_db, collection_name='default', embedding_function=embeddings)
-# second_db = Chroma(persist_directory=path_to_new_db, collection_name='copy_to', embedding_function=embeddings)
-#
-# first_embs = first_db.get(include=['embeddings'])['embeddings']
-# second_embs = second_db.get(include=['embeddings'])['embeddings']
-#
-# print(np.allclose(first_embs[0], second_embs[0])) # проверка на то, что эмбеддинги одинаковые
+first_db = Chroma(persist_directory=path_to_existing_db, collection_name='default', embedding_function=embeddings)
+second_db = Chroma(persist_directory=path_to_new_db, collection_name='copy_to2', embedding_function=embeddings)
+
+first_embs = first_db.get(include=['embeddings'])['embeddings']
+second_embs = second_db.get(include=['embeddings'])['embeddings']
+
+print(np.allclose(first_embs[0], second_embs[0])) # проверка на то, что эмбеддинги одинаковые
