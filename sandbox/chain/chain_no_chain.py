aa87bd747c996c0f49bbd5bc4c02bdf3159fbea1
A.Artyushin <artyush1n3@mail.ru>
2025-07-21 09:53:35 +0300

    Вызов LLM и поиск контекста вручную, создание ручного промпта

---
+++ sandbox/chain/chain_no_chain.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from os.path import normpath, join, dirname
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_gigachat import GigaChat, GigaChatEmbeddings
+
+load_dotenv()
+llm = GigaChat()
+path_to_db = normpath(join(dirname(__file__), '..', '..', 'chroma.kb'))
+db = Chroma(collection_name="kb.gigaRtext", embedding_function=GigaChatEmbeddings(model='Embeddings'), persist_directory=path_to_db)
+
+system_prompt = """Представь, что ты ассистент поддержки, отвечающий на важные
+    мне вопросы исключительно на русском языке, на основании контекста, написанного ниже.
+    Если информации нет в контексте, то напиши кратко: "Не могу помочь с этим вопросом" и ничего больше.
+    При ответе на вопрос также ни в коем случае не упоминай, 
+    что существует какой-то контекст. Отвечай языком Михаила Васильевича Ломоносова.
+    """
+
+def join(docs):
+    s = '\n\n'.join(doc.page_content for doc in docs)
+    return s
+
+prompt = ChatPromptTemplate.from_template(system_prompt)
+question = input("Введите ваш вопрос: ")
+
+context = join(db.similarity_search(query=question, k=5))
+
+answer = llm.invoke(system_prompt + '\n' + "Контекст: " + context + '\n' +
+                    'Вопрос: ' + question + '\n' + "Твой ответ: ")
+
+print(answer.content)
