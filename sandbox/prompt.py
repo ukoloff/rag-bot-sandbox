007de7ad1062cbe35ed982e77206a0c329b8e8f6
A.Artyushin <artyush1n3@mail.ru>
2025-07-21 13:53:10 +0300

    formatted

--- sandbox/prompt.py
+++ sandbox/prompt.py
@@ -5,21 +5,21 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda
 from os.path import normpath, join, dirname
 
-path_to_file = normpath(join(dirname(__file__), 'prompt.txt'))
+path_to_file = normpath(join(dirname(__file__), "prompt.txt"))
 system_prompt = """
     Контекст: {context}
     Вопрос: {question}
     Твой ответ: """
 
-input = {'context': 'Контекст', 'question': 'Вопрос'}
+input = {"context": "Контекст", "question": "Вопрос"}
 
 def my_prompt(data):
-    with open(file=path_to_file ,encoding='UTF-8') as f:
+    with open(file=path_to_file, encoding="UTF-8") as f:
         text = f.read()
     return ChatPromptTemplate.from_template(text + system_prompt).invoke(data)
 
 async def amy_prompt(data):
-    async with aiofiles.open(file=path_to_file ,encoding='UTF-8') as f:
+    async with aiofiles.open(file=path_to_file, encoding="UTF-8") as f:
         text = await f.read()
     return await ChatPromptTemplate.from_template(text + system_prompt).ainvoke(data)
 
