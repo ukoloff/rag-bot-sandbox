f54e15e2d607f5c723f307f77e562da0abb2e6e3
A.Artyushin <artyush1n3@mail.ru>
2025-07-21 09:52:58 +0300

    изменено комментирование

--- sandbox/tgbot/collection_deleting.py
+++ sandbox/tgbot/collection_deleting.py
@@ -7,10 +7,10 @@ pdf_folder_path = normpath(join(dirname(__file__), '..', '..', 'pdfs'))
 db_path = normpath(join(dirname(__file__), '..', '..', 'chroma.kb'))
 client = chromadb.PersistentClient(path=db_path)
 collection = client.get_collection(name='kb.gigaR')
-# print(collection.get(include=['metadatas']))
+print(collection.get(include=['metadatas']))
 
-for file in os.listdir(pdf_folder_path):
-    if file.endswith('.pdf'):
-        file_path = join(pdf_folder_path, file)
-        print(file_path)
-        collection.delete(where={'source': file_path})
+# for file in os.listdir(pdf_folder_path):
+#     if file.endswith('.pdf'):
+#         file_path = join(pdf_folder_path, file)
+#         print(file_path)
+#         collection.delete(where={'source': file_path})
