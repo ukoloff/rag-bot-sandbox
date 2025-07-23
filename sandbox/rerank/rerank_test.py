3c166d965cd80030181e45cc1f29c59f6f221c4b
A.Artyushin <artyush1n3@mail.ru>
2025-07-23 09:48:46 +0300

    Вывод контекста через ретривер

---
+++ sandbox/rerank/rerank_test.py
@@ -0,0 +1,17 @@
+from os.path import normpath, join, dirname
+from langchain_chroma import Chroma
+from langchain_gigachat import GigaChat, GigaChatEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+path_to_db = normpath(join(dirname(__file__), '..', '..', 'chroma.kb'))
+db = Chroma(collection_name="kb.gigaRtext", embedding_function=GigaChatEmbeddings(model='Embeddings'), persist_directory=path_to_db)
+
+def join_context(docs):
+    s = '\n\n'.join(doc.page_content for doc in docs)
+    return s
+
+question = input()
+context = join_context(db.similarity_search(query=question, k=5))
+print(context)
